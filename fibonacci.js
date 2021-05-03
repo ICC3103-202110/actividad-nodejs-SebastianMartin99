04ac26e0dfee0b72be3337343b22e851be3952fd
Sebastian Martin Muñoz <sebamm1999@gmail.com>
2021-05-03 11:52:55 -0400

    instalando paquete prompt-sync

--- fibonacci.js
+++ fibonacci.js
@@ -4,7 +4,7 @@ function fibonacci(num) {
     return fibonacci(num-1) + fibonacci(num-2)
 }
 
-const prompt = require("prompt-sync")(); // prompt es como el input en python, tuve que instalar en el terminal, puse: "npm install prompt-sync"
+const prompt = require("prompt-sync")(); // prompt es como el input en python, tuve que instalarlo. En el terminal puse: "npm install prompt-sync"
 const fib_num = prompt("Ingrese el número de la sucesión que desea saber: "); // Fué la unica manera que encontré para interactuar en la consola.
 console.log(fibonacci(Number(fib_num))); //Number() para pasar de texto a número
 
